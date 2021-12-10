6232c7d900b8824b6848a125a59982c5570cb733
Nam Truong <dinhnamtruong090900@gmail.com>
2021-12-10 11:41:38 +0700

    bai thuc hanh so 8

--- app/src/main/java/ie/app/Report.java
+++ app/src/main/java/ie/app/Report.java
@@ -1,29 +1,24 @@
 package ie.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import ie.app.R;
-public class Report extends AppCompatActivity
+public class Report extends Base
 {
     ListView listView;
-    static final String[] numbers = new String[] {
-            "Amount, Pay method",
-            "10, Direct",
-            "100, PayPal",
-            "1000, Direct",
-            "10, PayPal",
-            "5000, PayPal"};
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
         listView = (ListView) findViewById(R.id.reportList);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, numbers);
+        DonationAdapter adapter = new DonationAdapter(this, donations);
         listView.setAdapter(adapter);
     }
 }
